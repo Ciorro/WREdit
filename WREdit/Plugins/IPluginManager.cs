230ac3d79328faf7a13d7a85080dbb52c4faf6cc
Julian Zalewski <julzal2000@gmail.com>
2023-11-25 18:17:58 +0100

    Added plugin manager.

---
+++ WREdit/Plugins/IPluginManager.cs
@@ -0,0 +1,10 @@
+﻿using WREdit.Base.Actions;
+
+namespace WREdit.Plugins
+{
+    internal interface IPluginManager
+    {
+        IEnumerable<IGameObjectAction> Actions { get; }
+        void InitializePlugins();
+    }
+}
---
+++ WREdit/Plugins/PluginManager.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Reflection;
+using WREdit.Base.Actions;
+
+namespace WREdit.Plugins
+{
+    internal class PluginManager : IPluginManager
+    {
+        private readonly string _pluginsDirectory;
+        private IEnumerable<IGameObjectAction>? _actions;
+
+        public PluginManager(string pluginsDirectory)
+        {
+            _pluginsDirectory = Path.GetFullPath(pluginsDirectory);
+        }
+
+        public IEnumerable<IGameObjectAction> Actions
+        {
+            get => _actions ?? Enumerable.Empty<IGameObjectAction>();
+        }
+
+        public void InitializePlugins()
+        { 
+            var plugins = Directory.GetFiles(_pluginsDirectory, "*.dll").
+                          Select(Assembly.LoadFile);
+            _actions = plugins.SelectMany(InitializeActions);
+        }
+
+        private IEnumerable<IGameObjectAction> InitializeActions(Assembly plugin)
+        {
+            var types = plugin.GetTypes();
+
+            var actionTypes = types.Where(type =>
+            {
+                return type.IsAssignableTo(typeof(IGameObjectAction));
+            });
+
+            var actions = actionTypes.Select(actionType =>
+            {
+                return (IGameObjectAction)Activator.CreateInstance(actionType)!;
+            });
+
+            return actions.Where(action => action is not null);
+        }
+    }
+}
