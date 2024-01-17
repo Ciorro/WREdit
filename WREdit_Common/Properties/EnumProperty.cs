c8f58a4008798bfc9df92e2868ecf789974d4c18
Julian Zalewski <julzal2000@gmail.com>
2024-01-17 01:55:57 +0100

    Added enum property.

---
+++ WREdit_Common/Properties/EnumProperty.cs
@@ -0,0 +1,27 @@
+﻿using WREdit.Base.Attributes;
+using WREdit.Base.Processing.Properties;
+
+namespace WREdit.Common.Properties
+{
+    [PropertyTemplate("Properties/EnumPropertyTemplate.xaml")]
+    [TargetType(typeof(Enum))]
+    public class EnumProperty : ProcessorProperty
+    {
+        public EnumProperty(string propertyName, object instance)
+            : base(propertyName, instance)
+        { }
+
+        public System.Collections.IEnumerable? PossibleValues
+        {
+            get
+            {
+                if (Property.PropertyType.IsAssignableTo(typeof(Enum)))
+                {
+                    return Enum.GetValues(Property.PropertyType);
+                }
+
+                return null;
+            }
+        }
+    }
+}
