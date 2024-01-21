c27105012e5b07ab8a73de187ea0cda4c7c620d3
Julian Zalewski <julzal2000@gmail.com>
2024-01-21 22:48:25 +0100

    Create ObjectToVisibilityConverter.cs

---
+++ WREdit/Converters/ObjectToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WREdit.Converters
+{
+    internal class ObjectToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new ObjectToVisibilityConverter();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str && string.IsNullOrWhiteSpace(str))
+            {
+                return Visibility.Collapsed;
+            }
+
+            if (value == default)
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
